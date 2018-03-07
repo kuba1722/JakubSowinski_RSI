49001ec81ccb0ec32a39f3140e872193036c9978
kuba1722 <32569941+kuba1722@users.noreply.github.com>
2018-03-07 12:48:02 +0100

    scenariusz 3, klasy abstrakcyjne i interfejsy

---
+++ src/Pojazd.java
@@ -0,0 +1,13 @@
+public abstract class Pojazd  implements Print{
+    String rejestracja;
+    short rok;
+    int przebieg;
+    boolean czyWypozyczone;
+    String model;
+    double silnik;
+
+}
+
+
+
+
